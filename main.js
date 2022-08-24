882a31c7cd181b5d9113daba090ddede6a16c943
codevil <namhundred@naver.com>
2022-08-25 00:39:44 +0900

    refactor: update manifest 2 to 3

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const sample_dictionary = [
+  "빈값",
   "하나의 값을 입력받을 때",
   "공백으로 구분된 한 줄의 값들을 입력받을 때",
   "여러 줄의 값들을 입력받을 때",
@@ -7,67 +8,33 @@ const sample_dictionary = [
   "하나의 값 또는 공백으로 구분된 여러 값들을 여러 줄에 걸쳐 뒤죽박죽 섞여서 입력받을 때",
 ];
 const sample_object = {
-  sn0: `const input = require('fs').readFileSync("/dev/stdin").toString().trim();`,
-  sn1: `const input = require('fs').readFileSync("/dev/stdin").toString().trim().split(" ");`,
-  sn2: `const input = require('fs').readFileSync("/dev/stdin").toString().trim().split("\\n");`,
-  sn3: `const [n, ...arr] = require('fs').readFileSync("/dev/stdin").toString().trim().split(/\\s/);`,
-  sn4: `const [n, ...arr] = require('fs').readFileSync("/dev/stdin").toString().trim().split("\\n");`,
-  sn5: `const input = require('fs').readFileSync("/dev/stdin").toString().trim().split(/\\s/);
-  const n = input[0];
-  const n_arr = input.slice(1, n+1);
-  const [m, ...m_arr] = input.slice(n+1);`,
+  sn0: ``,
+  sn1: `const input = require('fs').readFileSync('/dev/stdin').toString().trim();`,
+  sn2: `const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ');`,
+  sn3: `const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\\n');`,
+  sn4: `const [n, ...arr] = require('fs').readFileSync('/dev/stdin').toString().trim().split(/\\s/);`,
+  sn5: `const [n, ...arr] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\\n');`,
+  sn6: `const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(/\\s/);
+const n = input[0];
+const n_arr = input.slice(1, n+1);
+const [m, ...m_arr] = input.slice(n+1);`,
 };
-
-function displayAnswer(outputValue, index) {
-  const code_result = document
-    .querySelector(".code_result")
-    .querySelector(`.run_result${index}`);
-  const answer_div = document.createElement("div");
-  const run_output = code_result.querySelector(`.run_output${index}`);
-  code_result.append(answer_div);
-  answer_div.classList.add(`answer_result${index}`);
-  answer_div.append(outputValue);
-
-  if (answer_div.innerHTML.trim() === run_output.innerHTML.trim()) {
-    answer_div.classList.add("correct");
-  } else {
-    answer_div.classList.add("wrong");
-  }
+function createInputText() {
+  const input_selector_wrapper = document
+    .querySelector(".content")
+    .querySelector(".row");
+  const input_selector = document.createElement("div");
+  input_selector_wrapper.append(input_selector);
+  input_selector.classList.add("input_selector");
+  sample_dictionary.forEach((element, index) => {
+    const button_item = document.createElement("button");
+    button_item.classList.add(`sn${index}`);
+    button_item.innerHTML = element;
+    input_selector.append(button_item);
+  });
 }
-function clearOutput() {
-  const code_result = document.querySelector(".code_result");
-  code_result.innerHTML = "";
-}
-function run(inputValue, index) {
-  const code = document.querySelector(".code_area").value;
-  const script = `
-    var console = {};
-      console.log = function (text) {
-      const code_result = document.querySelector(".code_result");
-      const code_evaluation = document.createElement("div");
-      const run_output = document.createElement("div");
-      const txt = document.createTextNode([...arguments].reduce((acc, cur)=>acc+cur+" ", ""));
-      
-      code_evaluation.classList.add("run_result${index}")
-      code_result.appendChild(code_evaluation);
-      code_evaluation.append(run_output);
-      run_output.appendChild(txt);
-      run_output.classList.add(\`run_output${index}\`)
-    }
-    require = function (fs){
-      return {
-        readFileSync : function(state){
-          return \`${inputValue}\`
-        }
-      }
-    }
-
-    ;`;
-  const fn = new Function(script + code);
-  console.log(fn());
-}
-function close_sample() {
-  document.querySelector(".sample_container").classList.add("displayNone");
+function close_example() {
+  document.querySelector(".input_selector").classList.add("displayNone");
 }
 function readInput() {
   const input_list = [];
@@ -83,69 +50,144 @@ function readOutput() {
     .forEach((element) => output_list.push(element.innerHTML));
   return output_list;
 }
-function sample_container() {
-  const button_container_parent = document.querySelector(".run_div");
-  const button_container = document.createElement("div");
-  button_container_parent.append(button_container);
-  button_container.classList.add("sample_container");
-  const close_button = document.createElement("button");
-  close_button.classList.add("code_sample_close_button");
-  button_container.append(close_button);
-  close_button.innerHTML = "❌";
-  sample_dictionary.forEach((element, index) => {
-    const button_item = document.createElement("button");
-    button_item.classList.add(`sn${index}`);
-    button_item.innerHTML = element;
-    button_container.append(button_item);
-  });
-  button_container.addEventListener("click", (target) => {
-    const sample_object_key = target.target.classList;
-    if (sample_object[sample_object_key]) {
-      document.querySelector(
-        ".code_area"
-      ).innerHTML = `${sample_object[sample_object_key]}`;
-      close_sample();
-    }
-    if (target.target.classList.value === "code_sample_close_button") {
-      close_sample();
-    }
-  });
+const IFRAMECSS = `body{margin:0px;}
+.table_row{display:flex;margin-top:45px;}
+.table_row__input, .table_row__output{width:50%;}
+code-input textarea, pre, code[class*="language-"], pre[class*="language-"] {width:100vw!important;
+  word-break: break-word;
+  white-space: pre-wrap;}
+  .token.operator{background:none;}
+code-input{margin:0px;height: 500px;width: 100vw;padding: 0px;}
+code-input textarea, pre{box-sizing: border-box;}
+.run_code {float: right;padding: 10px 15px;border: 0;color: #fff;background-color: #428bca;}
+[class*="run_wrapper"] {  border-bottom: 1px solid black; display: flex;  gap: 1rem;}
+[class*="answer_result"], [class*="code_evaluation"] {width:50%; word-break:break-all;  }
+[class*="run_wrapper"].correct {color: green;}
+[class*="run_wrapper"].wrong {color: red;}`;
+const IframeScript = `codeInput.registerTemplate( "code-input",  codeInput.templates.prism(Prism, [new codeInput.plugins.Indent()]));`;
+function createIframeEvent() {
+  const input_list = JSON.stringify(readInput());
+  const output_list = JSON.stringify(readOutput());
+  return `
+    <script>
+      const run =function(inputValue, index){
+        console.log = function(){
+          const body = document.querySelector(".code_output_wrapper");
+          const run_wrapper =document.createElement("div");
+          const code_evaluation = document.createElement("div");
+          const txt = document.createTextNode([...arguments].reduce((acc, cur)=> 
+          typeof cur==="object" ? acc+JSON.stringify(cur) : acc+cur+" ", ""));
+          body.append(run_wrapper)
+          run_wrapper.classList.add("run_wrapper"+index);
+          run_wrapper.append(code_evaluation);
+          code_evaluation.classList.add("code_evaluation"+index);
+          code_evaluation.appendChild(txt);
+        }
+
+        require = function(fs){
+          return {readFileSync : function(state){
+          return inputValue}}};
+        const value = document.querySelector("textarea").value;
+        const fn = new Function(value)
+        fn()  
+      }
+
+      const displayAnswer= function(outputValue,index){
+        
+        const code_evaluation = document.querySelector(".code_evaluation"+index);
+        const code_result = document.querySelector(".run_wrapper"+index);
+        const answer_div =  document.createElement("div");
+        code_result?.append(answer_div);
+        answer_div.classList.add("answer_result"+index);
+        answer_div.append(outputValue);
+        if (answer_div?.innerHTML.trim() === code_evaluation?.innerHTML.trim()) {
+          code_result.classList.add("correct");
+        } else {
+          code_result.classList.add("wrong");
+        }
+      }
+      const createAllResult= function(){
+        document.querySelector('.code_output_wrapper').innerHTML="";
+        ${input_list}.forEach((inputValue, index) =>
+        run(inputValue, index));
+        ${output_list}.forEach((outputValue, index)=>
+        displayAnswer(outputValue,index));
+      }
+      const run_code = document.querySelector(".run_code")
+          run_code.addEventListener("click", () => {
+            createAllResult()
+          }
+
+      );
+      window.addEventListener("keydown", ()=>{
+          if (event.ctrlKey && event.keyCode === 13) {
+            createAllResult()
+          }
+        })
+    </script>
+    `;
 }
-function container() {
-  const run_div = document.createElement("div");
-  run_div.classList.add("run_div");
-  const run_area = document.querySelector(".content").querySelector(".row");
-  run_area.append(run_div);
-
-  const code_area = document.createElement("textarea");
-  const code_run = document.createElement("button");
-  const code_result = document.createElement("div");
-  const code_answer = document.createElement("div");
-
-  const input_list = readInput();
-  const output_list = readOutput();
-  code_area.classList.add("code_area");
-  code_answer.classList.add("code_answer");
-  run_div.append(code_area);
-  run_div.append(code_answer);
-  run_div.append(code_result);
-  run_div.append(code_run);
-  code_run.classList.add("code_run");
-  code_run.innerHTML = `실행`;
-  code_result.classList.add("code_result");
-
-  code_run.addEventListener("click", () => {
-    clearOutput();
-    input_list.forEach((inputValue, index) =>
-      run(inputValue.toString(), index)
-    );
-    output_list.forEach((outputValue, index) =>
-      displayAnswer(outputValue.toString(), index)
-    );
-  });
+function createHeadText(script) {
+  return `<head>
+<meta charset="UTF-8" />
+<link id="import-theme" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/themes/prism.css"/>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/components/prism-core.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/plugins/autoloader/prism-autoloader.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/WebCoder49/code-input@1.1/code-input.css"/>
+<script src="https://cdn.jsdelivr.net/gh/WebCoder49/code-input@1.1/code-input.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/WebCoder49/code-input@1.1/plugins/indent.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/WebCoder49/code-input@1.1/plugins/autodetect.js"></script>
+<script>${script}</script> 
+</head>`;
 }
+
+function createBodyText(sample, css) {
+  return `<body><style>${css}</style>
+<code-input lang="JavaScript" value=${sample}></code-input>
+<script>codeInput.registerTemplate("code-input",codeInput.templates.prism(Prism));</script>
+
+<button class="run_code">실행(CTRL+ENTER)</button>
+<div class="table_row">
+<div class="table_row__input">입력값</div>
+<div class="table_row__output">정답</div>
+</div>  
+<div class="code_output_wrapper"></div>
+</body>`;
+}
+function createIframeHTML(head, body, event) {
+  return `<html lang="en">${head} ${body}${event}</html>`;
+}
+
+function createIframe(text) {
+  const iframe = document.createElement("iframe");
+  const iframe_wrapper = document
+    .querySelector(".content")
+    .querySelector(".row");
+  iframe_wrapper.append(iframe);
+  iframe.classList.add("run_div");
+  iframe.srcdoc = `${text}`;
+  iframe.frameBorder = "0";
+}
+
 function init() {
-  container();
-  sample_container();
+  createInputText();
+  const input_selector = document.querySelector(".input_selector");
+  input_selector.addEventListener("click", (target) => {
+    const sample_object_key = target.target.classList;
+    const iframeBodyText = createBodyText(
+      `"${sample_object[sample_object_key]}"`,
+      IFRAMECSS
+    );
+
+    const iframeEvent = createIframeEvent();
+    const iframeHeadText = createHeadText(IframeScript);
+    const iframeHTML = createIframeHTML(
+      iframeHeadText,
+      iframeBodyText,
+      iframeEvent
+    );
+    createIframe(iframeHTML);
+    close_example();
+  });
 }
 init();
